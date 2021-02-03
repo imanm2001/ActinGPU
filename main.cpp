2c6790bd9f78bec9b2b09b89fea01e8cfa94e467
Mousavi <sayediman.mousavi@yale.edu>
2021-02-02 20:38:20 -0500

    Second Commit

--- main.cpp
+++ main.cpp
@@ -19,15 +19,14 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-cl_device_id device_id = NULL;
-cl_platform_id platform_id = NULL;
-cl_int ret;
-cl_uint ret_num_platforms;
-cl_uint ret_num_devices;
+    cl_device_id device_id = NULL;
+    cl_platform_id platform_id = NULL;
+    cl_int ret;
+    cl_uint ret_num_platforms;
+    cl_uint ret_num_devices;
     cl_int err = CL_SUCCESS;
 
-    // Get (in numPlatforms) the number of OpenCL platforms available
-    // No platform ID will be return, since platforms is NULL
+
 
     ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
     if (CL_SUCCESS != ret)
@@ -39,25 +38,24 @@ cl_uint ret_num_devices;
 
     if (CL_SUCCESS != err)
     {
-           std::cout<<"Error: Device"<<std::endl;
+        std::cout<<"Error: Device"<<std::endl;
         return ret;
     }
     size_t stringLength = 0;
     ret = clGetDeviceInfo(device_id, CL_DEVICE_VERSION, 0, NULL, &stringLength);
-        if (CL_SUCCESS != ret)
-        {
-               std::cout<<"Error: Device Info"<<std::endl;
-            return ret;
-        }
+    if (CL_SUCCESS != ret)
+    {
+        std::cout<<"Error: Device Info"<<std::endl;
+        return ret;
+    }
 
-        // Now, that we know the device's version string length, we can allocate enough space before read it
-        std::vector<char> deviceVersion(stringLength);
 
-        // Read the device's version string
-        // The read value returned in deviceVersion
-        clGetDeviceInfo(device_id, CL_DEVICE_VERSION, stringLength, &deviceVersion[0], NULL);
+    std::vector<char> deviceVersion(stringLength);
 
-         std::cout<<":::"<<deviceVersion.data() <<std::endl;
-         return 0;
+
+    clGetDeviceInfo(device_id, CL_DEVICE_VERSION, stringLength, &deviceVersion[0], NULL);
+
+    std::cout<<":::"<<deviceVersion.data() <<std::endl;
+    return 0;
     return a.exec();
 }
